2bc834084ed6501e0000aaf29ff2910e09066ce4
tnougosso <tnougosso@irex.aretex.ca>
2024-01-27 16:54:59 +0100

    intitiation a express tp2

--- initiation à express/Server.js
+++ initiation à express/Server.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
+require("./db")
 const express = require('express')
 const userRoutes = require('./routes/User')
+const postRoutes = require('./routes/Post')
 
 
 const app = express()
+
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -12,6 +16,7 @@ app.use((req, res, next) => {
 
 
 app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("Listening on port: ", process.env.PORT)
--- initiation à express/controller/User.js
+++ initiation à express/controller/User.js
@@ -1,12 +1,24 @@
+const User = require("../models/User")
 
 // Récupérer tous les utilisateurs
-const getAllUsers = (req, res) => {
-    res.json({msg: "get all users !"})
+const getAllUsers = async (req, res) => {
+    const users = await User.find({}).sort({ createdAt: -1 })
+    res.status(200).json(users)
 }
 
 // Ajouter un utilisateur
-const createUser = (req, res) => {
-    res.json({msg: "post user !"})
+const createUser = async (req, res) => {
+
+    console.log(req.body)
+
+    try {
+        const { name, email, password } = req.body
+
+        const user = await User.create({ name, email, password })
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = {
