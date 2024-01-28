9eecd7485185cc28ae127db74d5654e3ca6c1d4d
tnougosso <tnougosso@irex.aretex.ca>
2024-01-28 16:25:03 +0100

    add delete and update routes !

--- initiation à express/controller/User.js
+++ initiation à express/controller/User.js
@@ -6,10 +6,64 @@ const getAllUsers = async (req, res) => {
     res.status(200).json(users)
 }
 
+// Récupérer un seul utilisateur
+const getUser = async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+
+        if (!user) {
+            res.status(404).json({ error: "User not found !" })
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// Metttre à jour un utilsateur
+const updateUser = async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const { body } = req
+
+
+        const user = await User.updateOne({ _id: id }, { $set: body })
+
+        if (user.modifiedCount === 0) {
+            res.status(404).json({ error: "User not found !" })
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// Supprimer un utilsateur
+const deleteUser = async (req, res) => {
+
+    try {
+        const { id } = req.params
+
+        const user = await User.deleteOne({ _id: id })
+
+        if (user.deletedCount === 0) {
+            res.status(404).json({ error: "User not found !" })
+        }
+
+        res.status(200).json({ message: "User is deleted successfully !", data: user })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+
 // Ajouter un utilisateur
 const createUser = async (req, res) => {
-
-    console.log(req.body)
 
     try {
         const { name, email, password } = req.body
@@ -23,5 +77,8 @@ const createUser = async (req, res) => {
 
 module.exports = {
     getAllUsers,
-    createUser
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser
 }
--- initiation à express/routes/User.js
+++ initiation à express/routes/User.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { getAllUsers, createUser } = require('../controller/User')
+const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('../controller/User')
 
 const router = express.Router()
 
 router.get("/", getAllUsers)
+router.get("/:id", getUser)
 router.post("/", createUser)
+router.patch("/:id", updateUser)
+router.delete("/:id", deleteUser)
 
 module.exports = router
