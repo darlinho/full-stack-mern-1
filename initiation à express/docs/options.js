f8cd8e22f2f0e1bfceef03f461bace3981be17bf
tnougosso <tnougosso@irex.aretex.ca>
2024-02-04 20:49:52 +0100

    add swagger options config

---
+++ initiation à express/docs/options.js
@@ -0,0 +1,28 @@
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Logone Digital Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "Logone Digital",
+                url: "https://logone-digital.com",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+module.exports = options
