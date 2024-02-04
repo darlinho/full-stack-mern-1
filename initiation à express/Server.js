9b29abbae9ef775937dd69c3fc4db37b708ba75d
tnougosso <tnougosso@irex.aretex.ca>
2024-02-04 20:37:24 +0100

    swagger documentation

--- initiation à express/Server.js
+++ initiation à express/Server.js
@@ -1,18 +1,28 @@
 require('dotenv').config()
 require("./db")
-const express = require('express')
-const userRoutes = require('./routes/User')
-const postRoutes = require('./routes/Post')
-
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUi = require("swagger-ui-express"),
+    userRoutes = require('./routes/User'),
+    postRoutes = require('./routes/Post'),
+    options = require('./docs/options');
 
 const app = express()
-
-app.use(express.json())
-
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
+app.use(express.json())
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, {
+        explorer: true,
+    })
+);
 
 
 app.use("/users", userRoutes)
--- initiation à express/routes/User.js
+++ initiation à express/routes/User.js
@@ -1,3 +1,134 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         name:
+ *           type: string
+ *           description: The name of the User
+ *         email:
+ *           type: string
+ *           description: Email of the User
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the book was added
+ *       example:
+ *         id: d5fE_asz
+ *         name: The New Turing Omnibus
+ *         email: test@gmail.com
+ *         createdAt: 2020-03-10T04:05:06.157Z
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: The users managing API
+ * /users/:
+ *   get:
+ *     summary: Lists all the users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The created user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ * /users/{id}:
+ *   get:
+ *     summary: Get the user by id
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The user id
+ *     responses:
+ *       200:
+ *         description: The user response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The user was not found
+ *   put:
+ *    summary: Update the user by the id
+ *    tags: [Users]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The user id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: The user was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      404:
+ *        description: The user was not found
+ *      500:
+ *        description: Some error happened
+ *   delete:
+ *     summary: Remove the user by id
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The user id
+ *
+ *     responses:
+ *       200:
+ *         description: The user was deleted
+ *       404:
+ *         description: The user was not found
+ */
+
 const express = require('express')
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('../controller/User')
 
