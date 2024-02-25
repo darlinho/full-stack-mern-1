d49688a8b7aa611c6895efe78dc8863f03415f35
tnougosso <tnougosso@irex.aretex.ca>
2024-02-25 21:50:08 +0100

    Form validation

--- learn react/src/App.jsx
+++ learn react/src/App.jsx
@@ -5,6 +5,16 @@ function App() {
 
   const [bgBlack, setbgBlack] = useState(true)
   const [color, setColor] = useState({ color: "white" })
+  const [search, setSearch] = useState("")
+
+
+  const [password, setPassword] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [confirmPasswordError, setConfirmPasswordError] = useState("")
+
+
 
 
   const showList = true
@@ -16,7 +26,7 @@ function App() {
     { id: 4, name: "Mangue" }
   ]
 
-  const showDialog = () => {
+  const toggleBackground = () => {
     setbgBlack(!bgBlack)
     if (color.color == "white") {
       setColor({ color: "black" })
@@ -24,22 +34,74 @@ function App() {
       setColor({ color: "white" })
     }
   }
+  const addToList = () => {
 
+  }
+
+  const passwordValidation = (value) => {
+
+    setPassword(value)
+
+    if (value.length < 8) {
+      setPasswordError('Le mot de passe doit etre supérrieur à 6 caractères !')
+    } else {
+      setPasswordError("")
+    }
+  }
+
+  const confirmationPasswordValidation = (value) => {
+
+    setConfirmPassword(value)
+
+    if (value !== password) {
+      setConfirmPasswordError('Les mots de passe sont différents !')
+    } else {
+      setConfirmPasswordError('')
+
+    }
+  }
 
   return (
     <div className={`${bgBlack ? 'bg-black' : ''}`} style={color}>
-      <p> {hello} </p>
+      <p> {search} </p>
       {showList &&
         <ul>
-          {fruits.map((fruit, index) => {
+          {fruits.map((fruit) => {
             return (
-              <li key={index}>{fruit.name}</li>
+              <li key={fruit.id}>{fruit.name}</li>
             )
           })}
         </ul>
       }
+      <div className='flex'>
+        <p>
+          <input
+            type="password"
+            value={password}
+            placeholder='Veuillez entrer un mot de passe'
+            onChange={(e) => passwordValidation(e.target.value)}
+          />
+          <span className='red'>{passwordError}</span>
+        </p>
+        {password}
 
-      <button onClick={() => showDialog()}>test</button>
+        <p>
+
+          <input
+            type="password"
+            value={confirmPassword}
+            placeholder='Confirmer votre mot de passe !'
+            onChange={(e) => confirmationPasswordValidation(e.target.value)}
+          />
+          <span className='red'>{confirmPasswordError}</span>
+        </p>
+        {confirmPassword}
+      </div>
+
+
+
+      <button onClick={() => toggleBackground()}>test</button>
+      <button onClick={addToList}>ajouter</button>
     </div>
   )
 }
